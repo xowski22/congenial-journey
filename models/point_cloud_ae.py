463d5e9104313ac9a192752cab991aab3160c5f3
xowski22 <xowski22@gmail.com>
2025-03-28 21:45:07 +0100

    .

--- models/point_cloud_ae.py
+++ models/point_cloud_ae.py
@@ -4,4 +4,15 @@ import torch.nn as nn
 
 class PointCloudEncoder(nn.Module):
     def __init__(self, latent_dim=256):
-        super(PointCloudEncoder, self).__init__()
+        super().__init__()
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 256, 1)
+        self.conv4 = nn.Conv1d(256, 512, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(256)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, latent_dim)
+        self.bn5 = nn.BatchNorm1d(256)
