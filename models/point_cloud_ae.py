7450e2e1ba536eea3f6dd12b36ef5880560dc637
xowski22 <xowski22@gmail.com>
2025-03-29 21:13:17 +0100

    Initial model structure

--- models/point_cloud_ae.py
+++ models/point_cloud_ae.py
@@ -16,3 +16,29 @@ class PointCloudEncoder(nn.Module):
         self.fc1 = nn.Linear(512, 256)
         self.fc2 = nn.Linear(256, latent_dim)
         self.bn5 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        x = x.transpose(2, 1)
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 512)
+        x = F.relu(self.bn5(self.fc1(x)))
+        x = self.fc2(x)
+
+        return x
+
+class PointCloudDecoder(nn.Module):
+    def __init__(self, latent_dim=256, num_points=2048):
+        super().__init__()
+        self.encoder = PointCloudEncoder(latent_dim)
+        self.decoder = PointCloudDecoder(latent_dim, num_points)
+
+    def forward(self, x):
+        lantent = self.encoder(x)
+        reconstruction = self.decoder(lantent)
+        return reconstruction, lantent
