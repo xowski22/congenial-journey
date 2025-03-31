05fe91431b0046082b47d01b690d9851b9503931
xowski22 <xowski22@gmail.com>
2025-03-31 20:45:03 +0200

    .

--- scripts/train_autoencoder.py
+++ scripts/train_autoencoder.py
@@ -19,4 +19,14 @@ def chamfer_distance(x, y):
     num_points_x = x.shape[1]
     num_points_y = y.shape[1]
 
+    x = x.unsqueeze(2)
+    y = y.unsqueeze(1)
 
+    dist = torch.sum((x-y)**2, dim=-1)
+
+    min_dist_x = torch.min(dist, dim=2)[0]
+    min_dist_y = torch.min(dist, dim=1)[0]
+
+    chamfer_dist = torch.mean(min_dist_x, dim=1) + torch.mean(min_dist_y, dim=1)
+
+    return torch.mean(chamfer_dist)
