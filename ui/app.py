4fc614153f5e8548b74c7151e5100b36efb64120
xowski22 <xowski22@gmail.com>
2025-04-19 21:28:18 +0200

    .

--- ui/app.py
+++ ui/app.py
@@ -5,3 +5,19 @@ import sys
 import os
 
 sys.path.append('.')
+
+
+class ModelInterface:
+    def __init__(self, model_path, model_type="vae", device="cuda"):
+        self.device = device
+        self.model_type = model_type
+
+        if model_type == "vae":
+            self.model = PointCloudVAE(latent_dim=256, num_points=2048).to(device)
+        elif model_type == "diffusion":
+            self.model = PointCloudDiffusion(num_points=2048).to(device)
+        else:
+            raise ValueError(f"Unknown model type: {model_type}")
+
+        self.model.load_state_dict(torch.load(model_path, map_location=device))
+        self.model.eval()
